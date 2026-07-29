fa6b724df50e139681cbafb69c15ec1a38ef646c
MPCoding93 <162565577+PauwelsFreelance@users.noreply.github.com>
2026-07-30 00:45:09 +0200

    Changes to be committed:

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pauwels/freelance-site',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cb2407b9bcdd5ebb66c8a11158267c4e7ddef8cc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pauwels/freelance-site' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cb2407b9bcdd5ebb66c8a11158267c4e7ddef8cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
